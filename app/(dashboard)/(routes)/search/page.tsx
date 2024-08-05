b7e3d997ee72c76b079985ddf719452df5c66cef
Gokarna Chaudhay <gokarnachy28@gmail.com>
2024-08-05 15:20:39 +0545

    dynamic layout : teacher mode and playmode

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,12 @@
+const SearchPage = () => {
+    return (  
+        <div>
+            search Page
+        </div>
+    );
+}
+ 
+export default SearchPage;
+<div>
+    search Page
+</div>
---
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import * as z from "zod"
+import axios from "axios"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/router"
+
+const CreatePage = () => {
+    return (
+        <div>
+            Create page
+        </div>
+    );
+}
+
+export default CreatePage;
